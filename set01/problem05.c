4f32dbe9260b29900bd2d42ba79d3bce4ab38a01
Vinayak17 <vinayakar.bsc23@rvu.edu.in>
2024-02-27 14:56:04 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+int input();
+int compare(int a,int b,int c);
+void output(int a,int b,int c,int largest);
+int main() {
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    largest=compare(a,b,c);
+    return 0;
+}
+int input()
+{
+    int x;
+    printf("Enter an integer: ");
+    scanf("%d",&x);
+    return x;
+}
+ int compare(int a,int b,int c)
+ {
+    int largest;
+    if(a>=b && a>=c)
+    {
+        largest=a;
+    }
+    else if(b>=a && b>=c)
+    {
+        largest=b;
+    }
+    else
+    {
+        largest=c;
+    }
+    else{
+        printf("numbers are equal");
+    }
+    return largest;
+ }
+ void output(int a,int b,int c,int largest)
+{
+    printf("\nLargest number is %d.",largest);
+}
 
